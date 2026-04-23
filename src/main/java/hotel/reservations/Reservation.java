998b7636d0f6c7f775bdaf6e9dd6286d0413df97
karim <karimoraby930@gmail.com>
2026-04-23 10:55:36 +0200

    initial commit

---
+++ src/main/java/hotel/reservations/Reservation.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hotel.reservations;
+
+import hotel.people.Guest;
+import hotel.rooms.Room;
+import hotel.model.ReservationStatus;
+import java.time.LocalDate;
+
+public class Reservation {
+    private int reservationId;
+    private Guest guest;
+    private Room room;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private ReservationStatus status;
+
+
+    public Reservation(int reservationId, Guest guest, Room room, LocalDate checkInDate, LocalDate checkOutDate) {
+        this.reservationId = reservationId;
+        this.guest = guest;
+        this.room = room;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.status = ReservationStatus.CONFIRMED;
+    }
+
+    public void cancel() {
+        this.status = ReservationStatus.CANCELLED;
+        room.free();
+        System.out.println("Reservation " + reservationId + " cancelled.");
+    }
+
+    public void complete() {
+        this.status = ReservationStatus.COMPLETED;
+        room.free();
+        System.out.println("Reservation " + reservationId + " completed.");
+    }
+
+    public int getReservationId() {
+        return reservationId;
+    }
+    public Guest getGuest() {
+        return guest;
+    }
+    public Room getRoom() {
+        return room;
+    }
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
+    public ReservationStatus getStatus() {
+        return status;
+    }
+
+    public void setCheckInDate(LocalDate checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+    public void setCheckOutDate(LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+}
+
+
