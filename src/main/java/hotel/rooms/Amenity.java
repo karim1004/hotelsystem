182597a0026a66254ee0ffddefa084f4e48cefaf
zeinahabdelhakim-web <zeinah.abdelhakim@gmail.com>
2026-04-26 17:20:11 +0300

    Refactor Amenity class and remove redundant code

--- src/main/java/hotel/rooms/Amenity.java
+++ src/main/java/hotel/rooms/Amenity.java
@@ -4,28 +4,6 @@
  */
 package hotel.rooms;
 
-import hotel.model.AmenityType;
-import hotel.exceptions.InvalidDataException;
-
-public class Amenity {
-    private AmenityType type;
-    private String description;
-
-    public Amenity(AmenityType type, String description) throws InvalidDataException{
-         if (type == null)
-            throw new InvalidDataException("Amenity type cannot be null.");
-        if (description == null || description.trim().isEmpty())
-            throw new InvalidDataException("Amenity description cannot be empty.");
-
-        this.type = type;
-        this.description = description;
-    }
-
-    public AmenityType getType() {
-        return type;
-    }
-    public void setType(AmenityType type) {
-        package hotel.rooms;
 
 import hotel.model.AmenityType;
 import hotel.exceptions.InvalidDataException;
@@ -68,22 +46,6 @@ public class Amenity {
         return type + " - " + description;
     }
 }
-        this.type = type;
-    }
 
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) throws InvalidDataException{
-        if (description == null || description.trim().isEmpty()) {
-            throw new InvalidDataException("Amenity description cannot be empty.");
-        }
-        this.description = description;
-    }
-
-    public String toString() {
-        return type + " - " + description;
-    }
-}
 
 
