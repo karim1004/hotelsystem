2b9b9d47aeca83b0ae8d24984dec7a6b970245a1
janaelsayedmohamedawad-cyber <janaelsayedmohamedawad@gmail.com>
2026-04-26 15:45:55 +0300

    Update Receptionist.java

--- src/main/java/hotel/people/Receptionist.java
+++ src/main/java/hotel/people/Receptionist.java
@@ -7,20 +7,27 @@ package hotel.people;
 import hotel.model.Gender;
 import hotel.model.Role;
 import java.time.LocalDate;
-
+import hotel.reservations.Reservation;
+import hotel.exceptions.InvalidDataException;
 public class Receptionist extends Staff {
 
     public Receptionist(String username, String password, LocalDate dob, String address, Gender gender, int workingHours) {
         super(username, password, dob, address, gender, Role.RECEPTIONIST, workingHours);
     }
-
-       public void checkIn(Reservation r) {
-        System.out.println("Guest checked in: " + r);
+public void checkIn(Reservation r) {
+        if (r == null)
+            throw new InvalidDataException("Reservation cannot be null.");
+        r.getRoom().occupy();
+        System.out.println("Guest checked in: " + r.getGuest().getUsername() +
+                           " -> Room " + r.getRoom().getRoomNumber());
     }
 
     public void checkOut(Reservation r) {
+        if (r == null)
+            throw new InvalidDataException("Reservation cannot be null.");
         r.complete();
-        System.out.println("Guest checked out.");
+        System.out.println("Guest checked out: " + r.getGuest().getUsername() +
+                           " from Room " + r.getRoom().getRoomNumber());
     }
    
 }
