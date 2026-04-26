43bfaddb11b00e42d07669517d362a8c1ffd90ee
janaelsayedmohamedawad-cyber <janaelsayedmohamedawad@gmail.com>
2026-04-26 15:27:06 +0300

    Update Staff.java

--- src/main/java/hotel/people/Staff.java
+++ src/main/java/hotel/people/Staff.java
@@ -6,14 +6,26 @@ package hotel.people;
 
 import hotel.model.Gender;
 import hotel.model.Role;
+import hotel.database.HotelDatabase;
+import hotel.exceptions.InvalidDataException;
 import java.time.LocalDate;
 
 public abstract class Staff extends Person {
     protected Role role;
     protected int workingHours;
+     protected Gender gender;
 
     public Staff(String username, String password, LocalDate dob, String address, Gender gender, Role role, int workingHours) {
         super(username, password, dob);
+        super(username, password, dob, address);
+        if (gender == null)
+            throw new InvalidDataException("Gender is required.");
+        if (role == null)
+            throw new InvalidDataException("Role is required.");
+         if (workingHours < 0)
+            throw new InvalidDataException("Working hours cannot be negative.");
+        this.gender = gender;
+         this.role = role;
         this.workingHours = workingHours;
     }
 
@@ -26,29 +38,49 @@ public abstract class Staff extends Person {
         }
     }
 
-
-    public void viewGuests() {
-        System.out.println("Viewing guest list...");
+     public void viewGuests() {
+        System.out.println("=== Guest List ===");
+        for (var g : HotelDatabase.getGuests())
+            System.out.println("  " + g);
     }
-    public void viewRooms() {
-        System.out.println("Viewing room list...");
+     public void viewRooms() {
+        System.out.println("=== Room List ===");
+        for (var r : HotelDatabase.getRooms())
+            System.out.println("  " + r);
     }
-    public void viewReservations() {
-        System.out.println("Viewing reservations...");
-    }
-
-
+     public void viewReservations() {
+        System.out.println("=== All Reservations ===");
+        for (var r : HotelDatabase.getReservations())
+            System.out.println("  " + r);
+     }
+   
     public Role getRole() {
         return role;
     }
     public void setRole(Role role) {
+        if (role == null)
+            throw new InvalidDataException("Role cannot be null.");
         this.role = role;
     }
+
     public int getWorkingHours() {
         return workingHours;
     }
-    public void setWorkingHours(int workingHours) {
+     public void setWorkingHours(int workingHours) {
+        if (workingHours < 0)
+            throw new InvalidDataException("Working hours cannot be negative.");
         this.workingHours = workingHours;
+    }
+    public Gender getGender() {
+        return gender;
+    }
+    public void setGender(Gender gender) {
+        if (gender == null)
+            throw new InvalidDataException("Gender is required.");
+        this.gender = gender;
+    }
+      public String toString() {
+        return role + "{username='" + username + "', workingHours=" + workingHours + "}";
     }
 }
 
