f6751e71ef501a4f49466f4a526600e4c5f2ef52
karim <80416952+karim1004@users.noreply.github.com>
2026-04-26 17:20:56 +0300

    Update InvalidDataException.java

--- src/main/java/hotel/exceptions/InvalidDataException.java
+++ src/main/java/hotel/exceptions/InvalidDataException.java
@@ -2,6 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package hotel.exceptions;
+
+
 public class InvalidDataException extends RuntimeException {
     public InvalidDataException(String message) {
         super(message);
